3227d36e231d71796f61c0877c423dc684f0e6de
Jackson Mitchell <jaxmit21@gmail.com>
2024-03-27 20:36:38 -0400

    Jsonbuilter Update
    changed json maker to use a csv as a import instead of a excel workbook
    
    make better css style for the table
    
    added the csv doc for keep sake

--- jsonBuliter.py
+++ jsonBuliter.py
@@ -1,45 +1,35 @@
+# json builder
 # Created By Jackson Mitchell
-# Last Updated: 2024-03-26
+# Last Updated: 2024-03-27
+# Creates a json file for a csv with first line of the 
+# csv are the keys and the subsequent rows are the corresponding values.
+# With each row being an object (excluding the first row)
 
-# !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+# For use change the csv and json names to your desired csv and json files
 
-# REMEMBER TO INCREASE THE ROW LOOP BY 1 WHEN YOU ADD A NEW TASK
+import json, csv
 
-# !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-
-# Creates a json file for my Technical Task Log Using a Excel Sheet
-
-import json
-import pandas as pd
-
-# Define variable to load the Excel Work book
-TechSheet = pd.read_excel('TaskSheet.xlsx', dtype='string')
-# Number of Full rows
-fullrows = 2
-  
 # Iterate the loop to read the cell values within the TimeSheet
-FirstRow = True
-HeaderList = []
-jsonList = []
-
-# Create a List of the Column Header
-for col in TechSheet:
-    HeaderList.append(col)
-
 # Loop thu each row given the number of full row
 # range(#) set to number of Logs
-for row in range(4):
-    TaskJson = {}
-    for col in TechSheet:
-        if col == HeaderList[3]:
-            TaskJson[col] = TechSheet[col][row][:-9]
+with open('TaskSheet.csv', newline='') as TaskSheet:
+
+    reader = csv.reader(TaskSheet, delimiter=',')
+
+    jsonList = []
+    # Loop thru rows then columns of the csv not including the first row
+    for i,row in enumerate(reader):
+        # print(i,row)
+        # Create a List of the Column Header
+        if i == 0:
+            HeaderList = row
         else:
-            TaskJson[col] = TechSheet[col][row]
-    jsonList.append(TaskJson)
+            TaskJson = {}
+            for ci,col in enumerate(row):
+                TaskJson[HeaderList[ci]] = col
+            jsonList.append(TaskJson)
 
-# print(HeaderList)
 print(jsonList)
-
 # Writing JSON data to a file
 with open("TaskSheet.json", "w") as json_file:
     json.dump(jsonList,json_file,indent=4)
